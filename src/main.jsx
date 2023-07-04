776cc339af2680b5ae9065cab00105ac2d35d68f
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2023-07-04 09:14:56 +0200

    sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'normalize.css';
+import '../src/styles/styles.scss'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
